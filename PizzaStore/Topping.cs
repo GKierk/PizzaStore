fd5903fda06c24f53b2813cf1176604044d52c02
Gabriel H. Kierkegaard <Gabriel_Kierkegaard@protonmail.com>
2023-04-07 10:10:20 +0200

    Implemented Topping class.

---
+++ PizzaStore/Topping.cs
@@ -0,0 +1,30 @@
+﻿namespace PizzaStore
+{
+    internal class Topping
+    {
+        #region Instance Fields
+        private string _name;
+        private int _price;
+        #endregion
+
+        #region Constructor
+        public Topping(string name, int price)
+        {
+            _name = name;
+            _price = price;
+        }
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public int Price
+        {
+            get { return _price; }
+        }
+        #endregion
+    }
+}
