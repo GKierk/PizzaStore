69ffa384bac4864feeb92cd99ebd50f6d1b09bd4
Gabriel H. Kierkegaard <Gabriel_Kierkegaard@protonmail.com>
2023-04-07 10:02:14 +0200

    Implemented Pizza class which inherits from abstract class MenuItem.

---
+++ PizzaStore/Pizza.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaStore
+{
+    public class Pizza : MenuItem
+    {
+        #region Constructor
+        public Pizza(string name, string size, int price) : base(name, size, price)
+        {
+
+        }
+        #endregion
+
+
+    }
+}
