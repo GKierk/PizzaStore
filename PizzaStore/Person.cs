f935833f1f0cd14fcc9ffe61d4413156a2e2cdc9
Gabriel H. Kierkegaard <Gabriel_Kierkegaard@protonmail.com>
2023-04-10 10:32:36 +0200

    Implemented abstract class person.

---
+++ PizzaStore/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace PizzaStore
+{
+    public abstract class Person
+    {
+        #region Instance Fields
+        string _name;
+        #endregion
+
+        #region Constructor
+        public Person(string name)
+        {
+            _name = name;
+        }
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        #endregion
+    }
+}
