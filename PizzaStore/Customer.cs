69cd8efabed6be2a5f3fcf46f6d0fcf2ba519761
Gabriel H. Kierkegaard <Gabriel_Kierkegaard@protonmail.com>
2023-04-10 10:44:16 +0200

    Implemented Customer class.

---
+++ PizzaStore/Customer.cs
@@ -0,0 +1,49 @@
+﻿namespace PizzaStore
+{
+    internal class Customer : Person
+    {
+        #region Instance Fields
+        List <MenuItem> _currentOrders = new List <MenuItem> ();
+        #endregion
+
+        #region Constructor
+        public Customer(string name) : base(name)
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        List<MenuItem> CurrentOrder
+        { 
+            get { return _currentOrders; }
+            set { _currentOrders = value; }
+        }
+        #endregion
+
+        #region Methods
+        public void ReadOrder()
+        {
+            string output = "Din ordre består af følgende: \n";
+
+            foreach(Pizza pizza in CurrentOrder)
+            {
+                output += $"\t{pizza.Name}\n";
+            }
+
+            Console.WriteLine(output);
+        }
+
+        public void AddPizzaToOrder(string pizzaName)
+        {
+            foreach(Pizza pizza in MenuCatalog.Pizzas)
+            {
+                if(pizza.Name.Equals(pizzaName))
+                {
+                    CurrentOrder.Add(pizza);
+                }
+            }
+        }
+        #endregion
+    }
+}
