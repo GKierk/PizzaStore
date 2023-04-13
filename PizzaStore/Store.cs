1f511b998955cfaecbe9dbbfbaa349a7456ae9a9
Gabriel H. Kierkegaard <gabriel_kierkegaard@protonmail.com>
2023-04-13 18:23:09 +0200

    Implemented Store class.

---
+++ PizzaStore/Store.cs
@@ -0,0 +1,30 @@
+﻿namespace PizzaStore
+{
+    public class Store
+    {
+        #region Instance Fields
+        private string _name;
+        #endregion
+
+        #region Constructor
+        public Store(string name)
+        {
+            _name = name;
+        }
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return _name; }
+        }
+        #endregion
+
+        #region Methods
+        public void Run()
+        {
+            UserDialog.RunDialog();
+        }
+        #endregion
+    }
+}
