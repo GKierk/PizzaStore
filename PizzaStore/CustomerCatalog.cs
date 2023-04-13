835df95bf5a7fc2dd8d73149b81521442de91862
Gabriel H. Kierkegaard <gabriel_kierkegaard@protonmail.com>
2023-04-13 21:17:04 +0200

    Implemented Choice method.

--- PizzaStore/CustomerCatalog.cs
+++ PizzaStore/CustomerCatalog.cs
@@ -67,6 +67,43 @@
             Console.WriteLine(_fullString);
         }
 
+        public static void Choice()
+        {
+            Console.WriteLine("Hvad ønsker du at foretage dig?" +
+                "\n1. Opret en ny kunde." +
+                "\n2. Få en liste over kunder." +
+                "\n3. Opdater Kunde." +
+                "\n4. Slet en kunde.");
+
+            string choice = Console.ReadLine();
+            int id = int.Parse(choice);
+
+            switch (id)
+            {
+                case 1:
+                    Console.WriteLine("Indtast oplysningerne i følgende rækkefølge.");
+                    Console.Write("Navn: ");
+                    string customerName = Console.ReadLine();
+                    CreateCustomer(customerName);
+                    break;
+                case 2:
+                    ReadCustomers();
+                    break;
+                case 3:
+                    UpdateCustomer();
+                    break;
+                case 4:
+                    ReadCustomers();
+                    Console.WriteLine("Skriv nummer på den pizza, som du ønsker at slette.");
+                    string readInput = Console.ReadLine();
+                    int deletionNumber = int.Parse(readInput);
+                    DeleteCustomer(deletionNumber);
+                    break;
+                default:
+                    break;
+            }
+        }
+
         public static void UpdateCustomer()
         {
             Console.WriteLine("Ønsker du at opdatere en kunde? Ja / Nej");
