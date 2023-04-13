9c49a30b27079e7fd531990405bde695b432eca8
Gabriel H. Kierkegaard <gabriel_kierkegaard@protonmail.com>
2023-04-13 16:42:38 +0200

    Implemented Order Class.

---
+++ PizzaStore/Order.cs
@@ -0,0 +1,61 @@
+﻿namespace PizzaStore
+{
+    public static class Order
+    {
+        #region Instance Fields
+
+        #endregion
+
+        #region Constructor
+        static Order()
+        {
+        }
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+        public static void CreateOrder(Customer customerName)
+        {
+            foreach (Customer id in CustomerCatalog.Customers)
+            {
+                if (id.Equals(customerName))
+                {
+                    MenuCatalog.ReadPizzas();
+                    Console.WriteLine("Hvilke pizza ønsker du at tilføje til din ordre? (Skriv navnet)");
+                    string input = Console.ReadLine();
+                    if (input is string)
+                    {
+                        customerName.AddPizzaToOrder(input);
+                    }
+                }
+            }
+        }
+
+        public static void ReadOrder(Customer customerName) 
+        {
+            string message = "";
+            message += "Du har bestilt det følgelnde:\n";
+
+            foreach(Pizza pizza in customerName.CurrentOrder)
+            {
+                message += $"\t{pizza.ToString()}\n";
+            }
+        }
+
+        public static double CalcultateTotal(Customer customerName)
+        {
+            double _total = 0;
+
+            foreach(MenuItem i in customerName.CurrentOrder)
+            {
+                _total += i.Price * Data.Tax;
+            }
+
+            return _total * Data.DeliveryFee;
+        }
+        #endregion
+    }
+}
