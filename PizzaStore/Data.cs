c90e2832bcdcedc6806c219ee4e4c6f0dcd1c53f
Gabriel H. Kierkegaard <gabriel_kierkegaard@protonmail.com>
2023-04-13 16:42:53 +0200

    Changed naming.

---
+++ PizzaStore/Data.cs
@@ -0,0 +1,29 @@
+﻿namespace PizzaStore
+{
+    public static class Data
+    {
+        #region Instance Fields
+        private static double _tax = 1.25;
+        private static double _deliveryFee = 40.00;
+        #endregion
+
+        #region Constructor
+        static Data()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        public static double Tax
+        {
+            get { return _tax; }
+        }
+
+        public static double DeliveryFee
+        {
+            get { return _deliveryFee; }
+        }
+        #endregion
+    }
+}
